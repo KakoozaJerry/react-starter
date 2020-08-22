7063f1a0e8e54efd8379008a9dc61e597945c07f
KakoozaJerry <abrahamkakooza@gmail.com>
2020-08-22 23:30:00 +0300

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home'
+import About from './About'
+import Services from './Services'
+import Contact from './Contact'
+import Nav from './Nav'
+import "bootstrap/dist/css/bootstrap.min.css"
+import {BrowserRouter, Route} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Route path='/' exact component={Home} />
+        <Route path='/about' exact component={About}/>
+        <Route path='/services' exact component={Services} />
+        <Route path='/contact' exact component={Contact} />
+
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/Services.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div>
+            <h1>This is the services module and component</h1>
+        </div>
+    )
+}
+
+export default Services
